ed390f47d6dcdd61ba51996ec59c9b8c858331c4
Jeannie Branstrator <jmbranstrator@gmail.com>
2022-06-23 11:01:23 -0500

    project init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import App from './components/App/App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//setup steps
+// -Import the library
+// -Plugin the middleware
+// -kickoff the sagas, watch for action types
+
+function* watcherSaga() {
+  // yield takeEvery ('SOME_ACTION', someFunction)
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  combineReducers({ 
+
+   }),
+  applyMiddleware(sagaMiddleware)
+);
+
+sagaMiddleware.run(watcherSaga)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
