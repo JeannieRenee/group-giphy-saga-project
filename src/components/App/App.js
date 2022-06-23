4a632f33afee1cef4dd124955ba12aaeeacad5d6
Badra Mohamed <moha1150@umn.edu>
2022-06-23 16:53:06 -0500

    favoritelist comp

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Favorite from '../Favorite/Favorite';
+import { Route, HashRouter as Router, Link } from 'react-router-dom';
+
+
 
 function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
+      <Router>
+        <Route>
+        <Favorite />
+        </Route>
+      </Router>
     </div>
   );
 }
